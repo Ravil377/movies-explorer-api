b9a9cb5f1ddcbce6b89155168c99ea121a3b6bf3
Ravil <ravil377@mail.ru>
2021-10-20 22:10:07 +0500

    cors

--- app.js
+++ app.js
@@ -27,17 +27,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
 
-// app.use(cors({
-//   origin: [
-//     'https://ravil377.nomoredomains.monster',
-//     'https://ravil377-front.nomoredomains.monster',
-//     'https://localhost:3000',
-//     'https://localhost:3001',
-//   ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: [
+    'https://ravil-movies-api.nomoredomains.monster',
+    'https://ravil-movies-frontend.nomoredomains.monster',
+  ],
+  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
